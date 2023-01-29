a5d525088787412cd16aaab67f56015a2a8612d2
Raj Kane <rkane10@asu.edu>
2023-01-29 16:38:20 -0700

    Update 55_canJump.py

--- 55_canJump.py
+++ 55_canJump.py
@@ -5,18 +5,18 @@ class Solution:
         Time:   	O(n*k) where k is max(nums)
         Space:  	O(n)
         '''
-        # if len(nums) == 1:
-        #     return True
-        # # track whether last index can be reached from each index 
-        # dp = [False for _ in nums]
-        # dp[-1] = True 
-        # for i in range(len(nums) - 2, -1, -1):
-        #     j = min(nums[i], len(nums) - i - 1)
-        #     while (not dp[i]) and j > 0:
-        #         if dp[i+j]:
-        #             dp[i] = True 
-        #         j -= 1
-        # return dp[0]
+        if len(nums) == 1:
+            return True
+        # track whether last index can be reached from each index 
+        dp = [False for _ in nums]
+        dp[-1] = True 
+        for i in range(len(nums) - 2, -1, -1):
+            j = min(nums[i], len(nums) - i - 1)
+            while (not dp[i]) and j > 0:
+                if dp[i+j]:
+                    dp[i] = True 
+                j -= 1
+        return dp[0]
 
         '''
 		Approach:	Greedy
