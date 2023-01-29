2b8f8eed3a9aaf5e51143fff849c2fe3fb97e95f
Raj Kane <rkane10@asu.edu>
2023-01-29 16:38:06 -0700

    Update 45_jump.py

--- 45_jump.py
+++ 45_jump.py
@@ -5,15 +5,15 @@ class Solution:
         Time:       O(n^2)
         Space:      O(n)
         '''
-        # if len(nums) == 1:
-        #     return 0
-        # dp = [len(nums) for _ in nums]
-        # dp[-1] = 0
-        # for i in range(len(nums) - 2, -1, -1):
-        #     max_step = min(nums[i], len(nums) - i - 1)
-        #     for j in range(max_step, 0, -1):
-        #         dp[i] = min(dp[i], dp[i+j] + 1)
-        # return dp[0]
+        if len(nums) == 1:
+            return 0
+        dp = [len(nums) for _ in nums]
+        dp[-1] = 0
+        for i in range(len(nums) - 2, -1, -1):
+            max_step = min(nums[i], len(nums) - i - 1)
+            for j in range(max_step, 0, -1):
+                dp[i] = min(dp[i], dp[i+j] + 1)
+        return dp[0]
 
         '''
         Approach:   Greedy / BFS
